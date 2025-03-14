93c541c624b2ece0c4669d5d5119ea4fea57d125
THH12062002 <thh12062002@gmail.com>
2025-03-14 17:12:39 +0700

    Refactor dashboard layout and loading states; remove unused files and update configuration

--- next.config.ts
+++ next.config.ts
@@ -1,12 +1,7 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  webpack: (config, { isServer }) => {
-    if (isServer) {
-      config.externals = [...(config.externals || []), "bcrypt"];
-    }
-    return config;
-  },
+  /* config options here */
 };
 
 export default nextConfig;
