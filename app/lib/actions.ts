2e442fe8cc86a8f0a93b7f3dacd2a1c83bebc613
THH12062002 <thh12062002@gmail.com>
2025-05-08 12:02:45 +0700

    Add @types/pg dependency and update pnpm-lock.yaml; simplify error handling in authentication

--- app/lib/actions.ts
+++ app/lib/actions.ts
@@ -122,12 +122,7 @@ export async function authenticate(
     await signIn("credentials", formData);
   } catch (error) {
     if (error instanceof AuthError) {
-      switch (error.type) {
-        case "CredentialsSignin":
-          return "Invalid credentials";
-        default:
-          return "Something went wrong";
-      }
+      return "Invalid credentials";
     }
     throw error;
   }
